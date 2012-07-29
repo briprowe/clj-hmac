f047a5c92080c6899b0c87fa68689e5b32c81b51
Brian Rowe <briprowe@yahoo.com>
2012-07-29 00:08:05 -0400

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-hmac
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_hmac/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-hmac.core-test
+  (:use clojure.test
+        clj-hmac.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
